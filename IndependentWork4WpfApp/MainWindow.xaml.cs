3e7d059039c4e20f77434a3b5be6137aadd51695
Андрей Маленьких <mrmaloy-1992@mail.ru>
2022-11-02 14:21:53 +1000

    Добавьте файлы проекта.

---
+++ IndependentWork4WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IndependentWork4WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            double rateDollar = Convert.ToDouble(rate1.Text);
+            double sumDollar = Convert.ToDouble(sum1.Text);
+            double resDouble = rateDollar * sumDollar;
+            resSum1.Text = resDouble.ToString();
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            double rateDollar = Convert.ToDouble(rate3.Text);
+            double sumDollar = Convert.ToDouble(sum3.Text);
+            double resDouble = rateDollar * sumDollar;
+            resSum3.Text = resDouble.ToString();
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            double rateDollar = Convert.ToDouble(rate4.Text);
+            double sumDollar = Convert.ToDouble(sum4.Text);
+            double resDouble = rateDollar * sumDollar;
+            resSum4.Text = resDouble.ToString();
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            double rateDollar = Convert.ToDouble(rate2.Text);
+            double sumDollar = Convert.ToDouble(sum2.Text);
+            double resDouble = rateDollar * sumDollar;
+            resSum2.Text = resDouble.ToString();
+        }
+
+        private void Button_Click5(object sender, RoutedEventArgs e)
+        {
+            double rateDollar = Convert.ToDouble(rate5.Text);
+            double d= 0.0254;
+            double resDouble = rateDollar * d;
+            resSum5.Text = resDouble.ToString();
+        }
+
+        private void Button_Click6(object sender, RoutedEventArgs e)
+        {
+            double rateDollar = Convert.ToDouble(rate6.Text);
+            double f = 0.3048;
+            double resDouble = rateDollar * f;
+            resSum6.Text = resDouble.ToString();
+        }
+
+        private void Button_Click7(object sender, RoutedEventArgs e)
+        {
+            double rateDollar = Convert.ToDouble(rate7.Text);
+            double m = 1609.34;
+            double resDouble = rateDollar * m;
+            resSum7.Text = resDouble.ToString();
+        }
+
+        private void Button_Click8(object sender, RoutedEventArgs e)
+        {
+            double rateDollar = Convert.ToDouble(rate8.Text);
+            double v = 1066.8;
+            double resDouble = rateDollar * v;
+            resSum8.Text = resDouble.ToString(); 
+        }
+    }
+}
